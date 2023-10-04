f0eab870ec4ab622cc797fd07a1e30ef271f9756
Emily Miller <emilyrauch3@gmail.com>
2023-10-04 12:56:14 -0400

    credit service created

---
+++ src/app/service/credit.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from "rxjs";
+import { Credit } from "src/model/credit.class";
+
+@Injectable({
+    providedIn: "root"
+})
+
+export class CreditService {
+    url: string = "http://localhost:8080/credits";
+
+    constructor(private http: HttpClient) { }
+
+    getAll(): Observable<Credit[]> {
+        return this.http.get(this.url) as Observable<Credit[]>;
+    }
+
+    getById(id: number): Observable<Credit> {
+        return this.http.get(this.url + "/" + id) as Observable<Credit>;
+    }
+
+    create(credit: Credit): Observable<Credit> {
+        return this.http.post(this.url, credit) as Observable<Credit>;
+    }
+
+    update(id: number): Observable<Credit> {
+        return this.http.put(this.url, id) as Observable<Credit>;
+    }
+
+    delete(id: number): Observable<Credit> {
+        return this.http.delete(this.url + "/" + id) as Observable<Credit>;
+    }
+}
