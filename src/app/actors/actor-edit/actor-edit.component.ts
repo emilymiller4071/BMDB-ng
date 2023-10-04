8fd021917f1938b7cc123574fa0a351c5649d88a
Emily Miller <emilyrauch3@gmail.com>
2023-10-04 09:42:50 -0400

    created actor-edit componenet

--- src/app/actors/actor-edit/actor-edit.component.ts
+++ src/app/actors/actor-edit/actor-edit.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Actor } from 'src/model/actor.class';
+import { ActorService } from 'src/app/service/actor.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-actor-edit',
   templateUrl: './actor-edit.component.html',
   styleUrls: ['./actor-edit.component.css']
 })
-export class ActorEditComponent {
+export class ActorEditComponent implements OnInit{
+  pageTitle: string = "Actor Edit";
+  actor: Actor = new Actor;
+  id: number = 0;
+
+  constructor(private actorService: ActorService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => this.id = params['id']);
+    this.actorService.getById(this.id).subscribe(jsonResponse => {
+      this.actor = jsonResponse as Actor;
+  });
+}
+
+  update() {
+    this.actorService.update(this.actor).subscribe(
+      (updatedActor: Actor) => {
+        // Update the local movie object with the new values returned from the server
+        this.actor = updatedActor;
+        // Redirect to movie detail page with updated movie object
+        this.router.navigate([`/actors/${this.actor.id}`]);
+      }
+    );
+  }
+
+  delete() {
+    this.actorService.delete(this.id).subscribe( jsonResponse =>
+      this.router.navigateByUrl('actors/list'));
+  }
 
 }
